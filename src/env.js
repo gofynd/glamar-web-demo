327196e86b0b515f23c45b4d796f778c1c776924
deepanshu-clould9 <deepanshujangid@gofynd.com>
2024-04-03 19:36:09 +0530

    code refector

--- src/env.js
+++
@@ -1,8 +0,0 @@
-const environment = window._conf || {
-  ENV: 'dev',
-  ASSET_URL: 'https://cdn-1.glamar.io/js/assets/v1.0.20',
-  SNELLO_URL: 'https://api.glamar.io/frieza',
-  MAKEUP_ASSET_URL: 'https://cdn-1.glamar.io/glamar_assets/web'
-};
-
-export default environment;
