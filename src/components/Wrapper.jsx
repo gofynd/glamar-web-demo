025bde7699d40164d987959cc82ced44b8281ab4
Rakesh Paihar <141932356+rakesh-singh-parihar@users.noreply.github.com>
2024-04-04 18:20:18 +0530

    Add files via upload

--- src/components/Wrapper.jsx
+++ src/components/Wrapper.jsx
@@ -1,38 +1,5 @@
 import { h } from 'preact';
-// import { useContext, useEffect } from 'preact/hooks';
-// import { getHomeSkuList } from '../utils';
-// import { GlobalContext, GlobalActionType } from './GlobalContextProvider';
-
 const Wrapper = ({ children }) => {
-  // const [, dispatch] = useContext(GlobalContext);
-  // useEffect(() => {
-  //   getHomeSkuList().then((data) => {
-  //     console.log(data.data);
-  //     dispatch({
-  //       type: GlobalActionType.UpdateHomeSkuList,
-  //       payload: { homeSkuList: data.data }
-  //     });
-  //     if (data.data[0]) {
-  //       dispatch({
-  //         type: GlobalActionType.UpdateTryOnType,
-  //         payload: { tryOnType: data?.data?.[0] }
-  //       });
-  //       dispatch({
-  //         type: GlobalActionType.UpdateTryOnTypeTabs,
-  //         payload: { tryOnTypeTabs: data?.data?.[0]?.tabs }
-  //       });
-  //       console.log(data?.data?.[0]?.tabs[0]);
-  //       dispatch({
-  //         type: GlobalActionType.UpdateActiveTryOnTab,
-  //         payload: { activeTryOnTab: data?.data?.[0]?.tabs[0] }
-  //       });
-  //       dispatch({
-  //         type: GlobalActionType.UpdateActiveSkuId,
-  //         payload: { activeSkuId: data?.data?.[0]?.tabs[0].skus[0].id }
-  //       });
-  //     }
-  //   });
-  // }, []);
   return <div className="container">{children}</div>;
 };
 
